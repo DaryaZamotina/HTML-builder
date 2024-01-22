6514d4b8131a20ca5bc3ffb14d8eb8b08a62c860
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-01-22 22:40:13 +0300

    feat: complete 05 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,41 +1,33 @@
 const readdir = require('fs');
 const fs = require('fs');
 const path = require('node:path'); 
+const writeFile = require('node:fs');
 
 const pathToStylesFolder = "./05-merge-styles/styles";
-
-fs.open('05-merge-styles/project-dist/bundle.css', 'w', (error) => {
-  if(error) throw error;
-  console.log('bundle.css is created!');
-});
 
 fs.readdir(pathToStylesFolder, {withFileTypes: true}, (error, files) => {
   if (error) {
     console.log(error);
     return;
   }
-  console.log("Styles files are as follows:");
- 
-  let arrStyles = [];
 
-  files.forEach(function(file) {
-    if (path.extname(file.name) == '.css') {
-        console.log(file.name);
+  let arrStyles = [];
+  let str;
+
+    files.forEach(function(file) {
+      if (path.extname(file.name) == '.css') {
         
         const pathToEachFile = path.join(pathToStylesFolder, `${file.name}`);
-        
-        for (const arrStyle of arrStyles) {
-          fs.readFile(pathToEachFile, "utf-8", (error, info) => {
+        fs.readFile(pathToEachFile, "utf-8", (error, info) => {
+          if (error) throw error;
+          arrStyles.push(info);
+          str = arrStyles.join('\n');
+
+          fs.writeFile('05-merge-styles/project-dist/bundle.css', str, (error) => {
             if (error) throw error;
-            arrStyle = info;
-          });
-        }
-    }
-  });
-
-  fs.writeFile('05-merge-styles/project-dist/bundle.css', arrStyles, (error) => {
-    if (error) throw error;
-    console.log("Everything is successfully signed to bundle.css");
-  })
-
+            console.log(`${file.name}` + " is successfully signed to bundle.css");
+          })
+        });
+      }
+    });
 });
