3c1a20bbf1998d85d0440733277e1e7229fcd036
Darya Zamotina <133055781+DaryaZamotina@users.noreply.github.com>
2024-01-22 23:34:29 +0300

    feat: add style.css

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const copyFile = require('fs');
+const readdir = require('fs');
+const writeFile = require('node:fs');
 
 fs.mkdir(path.join(__dirname, 'project-dist'), {recursive: true}, (error) => {
     if (error) {
@@ -8,6 +10,36 @@ fs.mkdir(path.join(__dirname, 'project-dist'), {recursive: true}, (error) => {
     }
     console.log("New directory project-dist has been created!")
 });
+
+const pathToStylesFolder = "./06-build-page/styles";
+
+fs.readdir(pathToStylesFolder, {withFileTypes: true}, (error, files) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  let arrStyles = [];
+  let str;
+
+    files.forEach(function(file) {
+      if (path.extname(file.name) == '.css') {
+        
+        const pathToEachFile = path.join(pathToStylesFolder, `${file.name}`);
+        fs.readFile(pathToEachFile, "utf-8", (error, info) => {
+          if (error) throw error;
+          arrStyles.push(info);
+          str = arrStyles.join('\n');
+
+          fs.writeFile('./06-build-page/project-dist/style.css', str, (error) => {
+            if (error) throw error;
+            console.log(`${file.name}` + " is successfully signed to style.css");
+          })
+        });
+      }
+    });
+});
+
 
 /*
 fs.readdir('./06-build-page/assets', {withFileTypes: true}, (error, files) => {
